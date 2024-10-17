49dbf2ed07192f86d73ce150c1d4169c94a24b90
L-Super <120311070@qq.com>
2024-10-17 23:31:38 +0800

    feat: add more test

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,6 @@
 #include "../SimZip.h"
-#include <catch2/reporters/catch_reporters_all.hpp>
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/reporters/catch_reporters_all.hpp>
 #include <filesystem>
 #include <fstream>
 
@@ -31,7 +31,8 @@ class CleanupListener : public Catch::EventListenerBase {
 public:
     using EventListenerBase::EventListenerBase;
 
-    void testRunEnded(Catch::TestRunStats const& testRunStats) override {
+    void testRunEnded(Catch::TestRunStats const& testRunStats) override
+    {
         // Perform cleaning after the test run is completed
         // 测试运行结束后执行清理
         clear();
@@ -68,38 +69,64 @@ TEST_CASE("create zip", "[create_zip]")
     {
         generateData(zhFileName);
 
-        SimZip zip("test/test3.zip");
-        zip.setmode(SimZip::OpenMode::Create);
+        SimZip zip("test/test3.zip", SimZip::OpenMode::Create);
         REQUIRE(zip.add(zhFileName) == true);
         REQUIRE(zip.add(zhFileName, "folder/文件.txt") == true);
         REQUIRE(zip.add(zhFileName, "文件夹/文件.txt") == true);
         REQUIRE(zip.add("不存在文件.txt") == false);
         zip.save();
     }
+
+    SECTION("create_zip4")
+    {
+        generateData(u8"中文文件.txt");
+
+        SimZip zip("test/test4.zip", SimZip::OpenMode::Create);
+        REQUIRE(zip.add(u8"中文文件.txt") == true);
+        REQUIRE(zip.add(u8"中文文件.txt", u8"folder/文件.txt") == true);
+        REQUIRE(zip.add(u8"中文文件.txt", u8"文件夹/文件.txt") == true);
+        zip.save();
+    }
 }
 
 TEST_CASE("extract zip", "[extract_zip]")
 {
+    const std::string outputPath{"test/output"};
     SECTION("Extract single file from zip")
     {
         SimZip zip("test/test1.zip", SimZip::OpenMode::Read);
-        zip.extract(enFilename, "test/output/");
-        REQUIRE(fs::exists("test/output/data.txt"));
+        zip.extract(enFilename, outputPath);
+        REQUIRE(fs::exists(outputPath + "/" + enFilename));
     }
 
     SECTION("Extract all files from zip")
     {
         SimZip zip("test/test1.zip", SimZip::OpenMode::Read);
-        zip.extractall("test/output/");
+        zip.extractall(outputPath);
         std::vector<std::string> expected_files = {enFilename, "folder/rename.txt"};
-        for (const auto& file: expected_files) { REQUIRE(fs::exists("test/output/" + file)); }
+        for (const auto& file: expected_files) { REQUIRE(fs::exists(outputPath + "/" + file)); }
     }
 
-    SECTION("Extract all files from zip")
+    SECTION("Extract single zh file from zip")
     {
         SimZip zip("test/test3.zip", SimZip::OpenMode::Read);
-        zip.extractall("test/output/");
+        zip.extract(zhFileName, outputPath);
+        REQUIRE(fs::exists(outputPath + "/" + zhFileName));
+    }
+
+    SECTION("Extract all zh files from zip")
+    {
+        SimZip zip("test/test3.zip", SimZip::OpenMode::Read);
+        zip.extractall(outputPath);
         std::vector<std::string> expected_files = {zhFileName, "folder/文件.txt", "文件夹/文件.txt"};
-        for (const auto& file: expected_files) { REQUIRE(fs::exists("test/output/" + file)); }
+        for (const auto& file: expected_files) { REQUIRE(fs::exists(outputPath + "/" + file)); }
+    }
+
+    SECTION("Extract all zh files from zip")
+    {
+        SimZip zip("test/test4.zip", SimZip::OpenMode::Read);
+        zip.extractall(outputPath);
+        std::vector<std::string> expected_files = {u8"中文文件.txt", u8"folder/文件.txt", u8"文件夹/文件.txt"};
+        for (const auto& file: expected_files) { REQUIRE(fs::exists(outputPath + "/" + file)); }
     }
 }
